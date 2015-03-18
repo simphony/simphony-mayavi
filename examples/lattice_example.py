633c03f8779aa3f55f1ac614d70ebc498c3ccb5f
Ioannis Tziakos <ioannist@enthought.com>
2015-03-18 20:28:35 +0000

    update lattice example to show data attributes

--- examples/lattice_example.py
+++ examples/lattice_example.py
@@ -1,10 +1,15 @@
 from mayavi.scripts import mayavi2
 
 from simphony.cuds.lattice import make_hexagonal_lattice
-
+from simphony.core.cuba import CUBA
 
 lattice = make_hexagonal_lattice('test', 0.1, (5, 4))
 
+temperature = 0.2
+for node in lattice.iter_nodes():
+    node.data[CUBA.TEMPERATURE] = temperature
+    lattice.update_node(node)
+    temperature += 0.2
 
 # Now view the data.
 @mayavi2.standalone
@@ -18,7 +23,8 @@ def view():
     g = Glyph()
     gs = g.glyph.glyph_source
     gs.glyph_source = gs.glyph_dict['sphere_source']
-    g.glyph.glyph.scale_factor = 0.05
+    g.glyph.glyph.scale_factor = 0.02
+    g.glyph.scale_mode = 'data_scaling_off'
     mayavi.add_module(g)  # noqa
 
 if __name__ == '__main__':
