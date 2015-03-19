8f969137777f892c632516d62415213849ae84f7
Ioannis Tziakos <ioannist@enthought.com>
2015-03-19 16:24:09 +0000

    add support for loading data information onto a Mayavi mesh source

--- simphony_mayavi/sources/tests/test_mesh_source.py
+++ simphony_mayavi/sources/tests/test_mesh_source.py
@@ -1,10 +1,14 @@
+import itertools
 import unittest
 
 import numpy
 from numpy.testing import assert_array_equal
 
+from simphony.core.cuba import CUBA
 from simphony.cuds.mesh import Mesh, Point, Cell, Edge, Face
+from simphony.core.data_container import DataContainer
 from simphony_mayavi.sources.api import MeshSource, cell_array_slicer
+
 from simphony_mayavi.sources.mesh_source import (
     CELL2VTKCELL, FACE2VTKCELL, EDGE2VTKCELL)
 
@@ -25,7 +29,10 @@ class TestParticlesSource(unittest.TestCase):
         self.edges = [[1, 4], [3, 8]]
         self.container = container = Mesh('test')
         self.point_uids = [
-            container.add_point(Point(coordinates=point)) for point in points]
+            container.add_point(
+                Point(coordinates=point,
+                      data=DataContainer(TEMPERATURE=index)))
+            for index, point in enumerate(points)]
 
     def test_points(self):
         container = self.container
@@ -36,15 +43,20 @@ class TestParticlesSource(unittest.TestCase):
         self.assertEqual(len(points), number_of_points)
         self.assertEqual(len(source.point2index), number_of_points)
 
+        self.assertEqual(source.data.point_data.number_of_arrays, 1)
+        temperature = source.data.point_data.get_array('TEMPERATURE')
         for key, index in source.point2index.iteritems():
             point = container.get_point(key)
             assert_array_equal(points[index], point.coordinates)
+            self.assertEqual(temperature[index], point.data[CUBA.TEMPERATURE])
 
     def test_cells(self):
         container = self.container
-        for cell in self.cells:
+        for i, cell in enumerate(self.cells):
             container.add_cell(
-                Cell(points=[self.point_uids[index] for index in cell]))
+                Cell(
+                    points=[self.point_uids[index] for index in cell],
+                    data=DataContainer(TEMPERATURE=i)))
 
         source = MeshSource.from_mesh(container)
         vtk_source = source.data
@@ -56,6 +68,8 @@ class TestParticlesSource(unittest.TestCase):
         self.assertEqual(len(cells), number_of_cells)
         self.assertEqual(len(source.element2index), number_of_cells)
 
+        self.assertEqual(source.data.cell_data.number_of_arrays, 1)
+        temperature = source.data.cell_data.get_array('TEMPERATURE')
         for key, index in source.element2index.iteritems():
             cell = container.get_cell(key)
             self.assertEqual(
@@ -63,12 +77,15 @@ class TestParticlesSource(unittest.TestCase):
                 CELL2VTKCELL[len(cell.points)])
             points = [source.point2index[uid] for uid in cell.points]
             self.assertEqual(cells[index], points)
+            self.assertEqual(temperature[index], cell.data[CUBA.TEMPERATURE])
 
     def test_edges(self):
         container = self.container
-        for edge in self.edges:
+        for i, edge in enumerate(self.edges):
             container.add_edge(
-                Edge(points=[self.point_uids[index] for index in edge]))
+                Edge(
+                    points=[self.point_uids[index] for index in edge],
+                    data=DataContainer(TEMPERATURE=i)))
 
         source = MeshSource.from_mesh(container)
         vtk_source = source.data
@@ -80,6 +97,8 @@ class TestParticlesSource(unittest.TestCase):
         self.assertEqual(len(edges), number_of_edges)
         self.assertEqual(len(source.element2index), number_of_edges)
 
+        self.assertEqual(source.data.cell_data.number_of_arrays, 1)
+        temperature = source.data.cell_data.get_array('TEMPERATURE')
         for key, index in source.element2index.iteritems():
             edge = container.get_edge(key)
             self.assertEqual(
@@ -87,12 +106,15 @@ class TestParticlesSource(unittest.TestCase):
                 EDGE2VTKCELL[len(edge.points)])
             points = [source.point2index[uid] for uid in edge.points]
             self.assertEqual(edges[index], points)
+            self.assertEqual(temperature[index], edge.data[CUBA.TEMPERATURE])
 
     def test_face(self):
         container = self.container
-        for face in self.faces:
+        for i, face in enumerate(self.faces):
             container.add_face(
-                Face(points=[self.point_uids[index] for index in face]))
+                Face(
+                    points=[self.point_uids[index] for index in face],
+                    data=DataContainer(TEMPERATURE=i)))
 
         source = MeshSource.from_mesh(container)
         vtk_source = source.data
@@ -104,6 +126,8 @@ class TestParticlesSource(unittest.TestCase):
         self.assertEqual(len(faces), number_of_faces)
         self.assertEqual(len(source.element2index), number_of_faces)
 
+        self.assertEqual(source.data.cell_data.number_of_arrays, 1)
+        temperature = source.data.cell_data.get_array('TEMPERATURE')
         for key, index in source.element2index.iteritems():
             face = container.get_face(key)
             self.assertEqual(
@@ -111,18 +135,26 @@ class TestParticlesSource(unittest.TestCase):
                 FACE2VTKCELL[len(face.points)])
             points = [source.point2index[uid] for uid in face.points]
             self.assertEqual(faces[index], points)
+            self.assertEqual(temperature[index], face.data[CUBA.TEMPERATURE])
 
     def test_all_element_types(self):
         container = self.container
+        count = itertools.count()
         for face in self.faces:
             container.add_face(
-                Face(points=[self.point_uids[index] for index in face]))
+                Face(
+                    points=[self.point_uids[index] for index in face],
+                    data=DataContainer(TEMPERATURE=next(count))))
         for edge in self.edges:
             container.add_edge(
-                Edge(points=[self.point_uids[index] for index in edge]))
+                Edge(
+                    points=[self.point_uids[index] for index in edge],
+                    data=DataContainer(TEMPERATURE=next(count))))
         for cell in self.cells:
             container.add_cell(
-                Cell(points=[self.point_uids[index] for index in cell]))
+                Cell(
+                    points=[self.point_uids[index] for index in cell],
+                    data=DataContainer(TEMPERATURE=next(count))))
 
         source = MeshSource.from_mesh(container)
         vtk_source = source.data
@@ -135,6 +167,8 @@ class TestParticlesSource(unittest.TestCase):
             len(self.faces) + len(self.edges) + len(self.cells)
         self.assertEqual(len(elements), number_of_elements)
         self.assertEqual(len(source.element2index), number_of_elements)
+        self.assertEqual(source.data.cell_data.number_of_arrays, 1)
+        temperature = source.data.cell_data.get_array('TEMPERATURE')
         for key, index in source.element2index.iteritems():
             cell_type = vtk_source.get_cell_type(index)
             if cell_type in EDGE2VTKCELL.values():
@@ -153,3 +187,5 @@ class TestParticlesSource(unittest.TestCase):
                 self.fail('vtk source has an unknown cell type')
             points = [source.point2index[uid] for uid in element.points]
             self.assertEqual(elements[index], points)
+            self.assertEqual(
+                temperature[index], element.data[CUBA.TEMPERATURE])
