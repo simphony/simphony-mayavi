b50329fec02cd2bfda17bdd73d6f0499c6659883
Ioannis Tziakos <ioannist@enthought.com>
2015-03-26 17:58:39 +0000

    add utility classes to the sources.api module

--- simphony_mayavi/sources/api.py
+++ simphony_mayavi/sources/api.py
@@ -1,10 +1,14 @@
 from .particles_source import ParticlesSource
 from .lattice_source import LatticeSource
 from .mesh_source import MeshSource
+from .cuds_data_accumulator import CUDSDataAccumulator
+from .cuds_data_extractor import CUDSDataExtractor
 from .utils import cell_array_slicer
 
 __all__ = [
     'ParticlesSource',
     'LatticeSource',
     'MeshSource',
+    'CUDSDataExtractor',
+    'CUDSDataAccumulator',
     'cell_array_slicer']
