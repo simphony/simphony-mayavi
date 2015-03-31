afd61c651fc9f8cbdad6a5059304d1fe00642798
Ioannis Tziakos <ioannist@enthought.com>
2015-03-31 03:35:21 +0100

    make sure that we start the mayavi engine

--- simphony_mayavi/snapshot.py
+++ simphony_mayavi/snapshot.py
@@ -33,47 +33,54 @@ def snapshot(cuds, filename):
     else:
         # The OffScreenEngine does not reliably work for linux/mac-os
         engine = Engine()
+
     engine.start()
+    try:
+        if isinstance(cuds, ABCMesh):
+            source = MeshSource.from_mesh(cuds)
+            window = engine.new_scene()
+            engine.add_source(source)
+            engine.add_module(Surface())
+        elif isinstance(cuds, ABCParticles):
+            source = ParticlesSource.from_particles(cuds)
+            engine = OffScreenEngine()
+            engine.start()
+            window = engine.new_scene()
+            engine.add_source(source)
+            module = Glyph()
+            glyph_source = module.glyph.glyph_source
+            glyph_source.glyph_source = \
+                glyph_source.glyph_dict['sphere_source']
+            module.glyph.glyph.scale_factor = \
+                _typical_distance(source.data) * 0.5
+            module.glyph.scale_mode = 'data_scaling_off'
+            engine.add_module(module)
+            module = Surface()
+            module.actor.mapper.scalar_visibility = False
+            engine.add_module(module)
+        elif isinstance(cuds, ABCLattice):
+            source = LatticeSource.from_lattice(cuds)
+            engine = OffScreenEngine()
+            engine.start()
+            window = engine.new_scene()
+            engine.add_source(source)
+            module = Glyph()
+            glyph_source = module.glyph.glyph_source
+            glyph_source.glyph_source = \
+                glyph_source.glyph_dict['sphere_source']
+            module.glyph.glyph.scale_factor = \
+                _typical_distance(source.data) * 0.5
+            module.glyph.scale_mode = 'data_scaling_off'
+            engine.add_module(module)
+        else:
+            msg = 'Provided object {} is not of any known cuds type'
+            raise TypeError(msg.format(type(cuds)))
 
-    if isinstance(cuds, ABCMesh):
-        source = MeshSource.from_mesh(cuds)
-        window = engine.new_scene()
-        engine.add_source(source)
-        engine.add_module(Surface())
-    elif isinstance(cuds, ABCParticles):
-        source = ParticlesSource.from_particles(cuds)
-        engine = OffScreenEngine()
-        engine.start()
-        window = engine.new_scene()
-        engine.add_source(source)
-        module = Glyph()
-        glyph_source = module.glyph.glyph_source
-        glyph_source.glyph_source = glyph_source.glyph_dict['sphere_source']
-        module.glyph.glyph.scale_factor = _typical_distance(source.data) * 0.5
-        module.glyph.scale_mode = 'data_scaling_off'
-        engine.add_module(module)
-        module = Surface()
-        module.actor.mapper.scalar_visibility = False
-        engine.add_module(module)
-    elif isinstance(cuds, ABCLattice):
-        source = LatticeSource.from_lattice(cuds)
-        engine = OffScreenEngine()
-        engine.start()
-        window = engine.new_scene()
-        engine.add_source(source)
-        module = Glyph()
-        glyph_source = module.glyph.glyph_source
-        glyph_source.glyph_source = glyph_source.glyph_dict['sphere_source']
-        module.glyph.glyph.scale_factor = _typical_distance(source.data) * 0.5
-        module.glyph.scale_mode = 'data_scaling_off'
-        engine.add_module(module)
-    else:
-        msg = 'Provided object {} is not of any known cuds type'
-        raise TypeError(msg.format(type(cuds)))
-
-    # Use some default values for the camera
-    camera = window.scene.camera
-    camera.azimuth(45)
-    camera.elevation(30)
-    window.scene.reset_zoom()
-    window.scene.save(filename, size)
+        # Use some default values for the camera
+        camera = window.scene.camera
+        camera.azimuth(45)
+        camera.elevation(30)
+        window.scene.reset_zoom()
+        window.scene.save(filename, size)
+    finally:
+            engine.stop()
