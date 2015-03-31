6ac16c200d632843d2b4d378c504208e2452705a
Ioannis Tziakos <ioannist@enthought.com>
2015-03-31 02:10:06 +0100

    rework snapshot function to really not create a popup

--- simphony_mayavi/snapshot.py
+++ simphony_mayavi/snapshot.py
@@ -1,5 +1,6 @@
-from mayavi import mlab
+from mayavi.api import OffScreenEngine
 from mayavi.tools.tools import _typical_distance
+from mayavi.modules.api import Glyph, Surface
 
 from simphony.cuds.abstractmesh import ABCMesh
 from simphony.cuds.abstractparticles import ABCParticles
@@ -22,27 +23,48 @@ def snapshot(cuds, filename):
          The filename to use for the output file.
 
     """
+    size = 800, 600
     if isinstance(cuds, ABCMesh):
         source = MeshSource.from_mesh(cuds)
-        mlab.options.offsceen = True
-        mlab.pipeline.surface(source, name=cuds.name)
+        engine = OffScreenEngine()
+        engine.start()
+        window = engine.new_scene()
+        engine.add_source(source)
+        engine.add_module(Surface())
     elif isinstance(cuds, ABCParticles):
         source = ParticlesSource.from_particles(cuds)
-        scale_factor = _typical_distance(source.data) * 0.5
-        mlab.options.offsceen = True
-        mlab.pipeline.glyph(
-            source, name=cuds.name,
-            scale_factor=scale_factor, scale_mode='none')
-        surface = mlab.pipeline.surface(source)
-        surface.actor.mapper.scalar_visibility = False
+        engine = OffScreenEngine()
+        engine.start()
+        window = engine.new_scene()
+        engine.add_source(source)
+        module = Glyph()
+        glyph_source = module.glyph.glyph_source
+        glyph_source.glyph_source = glyph_source.glyph_dict['sphere_source']
+        module.glyph.glyph.scale_factor = _typical_distance(source.data) * 0.5
+        module.glyph.scale_mode = 'data_scaling_off'
+        engine.add_module(module)
+        module = Surface()
+        module.actor.mapper.scalar_visibility = False
+        engine.add_module(module)
     elif isinstance(cuds, ABCLattice):
         source = LatticeSource.from_lattice(cuds)
-        scale_factor = _typical_distance(source.data) * 0.5
-        mlab.options.offsceen = True
-        mlab.pipeline.glyph(
-            source, name=cuds.name,
-            scale_factor=scale_factor, scale_mode='none')
+        engine = OffScreenEngine()
+        engine.start()
+        window = engine.new_scene()
+        engine.add_source(source)
+        module = Glyph()
+        glyph_source = module.glyph.glyph_source
+        glyph_source.glyph_source = glyph_source.glyph_dict['sphere_source']
+        module.glyph.glyph.scale_factor = _typical_distance(source.data) * 0.5
+        module.glyph.scale_mode = 'data_scaling_off'
+        engine.add_module(module)
     else:
         msg = 'Provided object {} is not of any known cuds type'
         raise TypeError(msg.format(type(cuds)))
-    mlab.savefig(filename)
+
+    # Use some default values for the camera
+    camera = window.scene.camera
+    camera.azimuth(45)
+    camera.elevation(30)
+    window.scene.reset_zoom()
+    window.scene.save(filename, size)
