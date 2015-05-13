92ffa8b91e1a19202527b403b90a95fc2eaa75a3
Ioannis Tziakos <ioannist@enthought.com>
2015-05-13 15:15:17 +0100

    expose CellCollection in the api module

--- simphony_mayavi/core/api.py
+++ simphony_mayavi/core/api.py
@@ -1,5 +1,5 @@
 from .cuba_data import CubaData
-from .utils import supported_cuba
+from .cuba_utils import supported_cuba
+from .cell_collection import CellCollection
 
-
-__all__ = ["CubaData", "supported_cuba"]
+__all__ = ["CubaData", "supported_cuba", "CellCollection"]
