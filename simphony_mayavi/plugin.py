4a782026c4a43fb9e42d9818da4918e388c64b8a
Ioannis Tziakos <ioannist@enthought.com>
2015-07-01 17:19:04 +0100

    add VTKMesh to the simphony plugin tools

--- simphony_mayavi/plugin.py
+++ simphony_mayavi/plugin.py
@@ -1,6 +1,7 @@
 from simphony_mayavi._version import full_version as __version__
 from simphony_mayavi.show import show
 from simphony_mayavi.snapshot import snapshot
-from simphony_mayavi.cuds.api import VTKParticles, VTKLattice
+from simphony_mayavi.cuds.api import VTKParticles, VTKLattice, VTKMesh
 
-__all__ = ['show', 'snapshot', '__version__', 'VTKParticles', 'VTKLattice']
+__all__ = [
+    'show', 'snapshot', '__version__', 'VTKParticles', 'VTKLattice', 'VTKMesh']
