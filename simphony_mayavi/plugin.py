8f3cc6d860d740377b1fce9e11f484ad06821963
Ioannis Tziakos <ioannist@enthought.com>
2015-07-07 22:00:31 +0100

    update plugin module

--- simphony_mayavi/plugin.py
+++ simphony_mayavi/plugin.py
@@ -1,8 +1,8 @@
 from simphony_mayavi._version import full_version as __version__
-from simphony_mayavi.api import show, snapshot, adapt2cuds
+from simphony_mayavi.api import show, snapshot, adapt2cuds, load
 from simphony_mayavi.cuds.api import VTKParticles, VTKLattice, VTKMesh
 
 __all__ = [
-    'show', 'snapshot', 'adapt2cuds',
+    'show', 'snapshot', 'adapt2cuds', 'load',
     '__version__',
     'VTKParticles', 'VTKLattice', 'VTKMesh']
