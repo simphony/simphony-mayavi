770478d439eadb15eb3ddebf4d4167a75c8b8d1d
Kit Yan Choi <kchoi@enthought.com>
2015-11-25 16:30:24 +0000

    selected a suite of test cases for lattice only

---
+++ simphony_mayavi/tests/lattice_only_tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+from simphony_mayavi.tests.lattice_only_tests.test_adapt2cuds import TestAdapt2Cuds
+from simphony_mayavi.tests.test_load import TestLoad
+from simphony_mayavi.tests.test_plugin_loading import TestPluginLoading
+from simphony_mayavi.tests.test_show import TestShow
+from simphony_mayavi.tests.test_snapshot import TestSnapShot
+
+def load_tests(loader, tests, pattern):
+    # customised to remove particle and mesh
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAdapt2Cuds)
+
+    suite.addTest(TestLoad("test_load_vase_1comp_vti"))
+    suite.addTest(TestPluginLoading("test_import"))
+    suite.addTest(TestPluginLoading("test_function_api"))
+    suite.addTest(TestShow("test_lattice_show"))
+    suite.addTest(TestSnapShot("test_lattice_snapshot"))
+    return suite
---
+++ simphony_mayavi/tests/lattice_only_tests/test_adapt2cuds.py
@@ -0,0 +1,50 @@
+import unittest
+
+from numpy import array, random
+from tvtk.api import tvtk
+from hypothesis import given
+from hypothesis.strategies import sampled_from
+
+from simphony.core.cuba import CUBA
+from simphony.cuds.abc_lattice import ABCLattice
+from simphony_mayavi.api import adapt2cuds
+
+from simphony_mayavi.tests.test_adapt2cuds import (create_image_data,
+                                                   create_poly_data)
+
+good_data_sets = sampled_from([
+    (create_image_data(array_name='TEMPERATURE'), 'lattice')])
+
+bad_array_data_sets = sampled_from([
+    (create_image_data(), 'lattice')])
+
+auto_data_sets = sampled_from([
+    (create_image_data(array_name='TEMPERATURE'), 'lattice')])
+
+expected = {'lattice': ABCLattice}
+
+
+class TestAdapt2Cuds(unittest.TestCase):
+
+    @given(good_data_sets)
+    def test_adapt_data_set(self, setup):
+        data_set, kind = setup
+        container = adapt2cuds(data_set, 'test', kind)
+        self.assertIsInstance(container, expected[kind])
+        self.assertIs(container.data_set, data_set)
+
+    @given(bad_array_data_sets)
+    def test_adapt_data_set_with_array_rename(self, setup):
+        data_set, kind = setup
+        container = adapt2cuds(
+            data_set, 'test', kind,
+            rename_arrays={'scalars': CUBA.TEMPERATURE})
+        self.assertIsInstance(container, expected[kind])
+        self.assertIsNot(container.data_set, data_set)
+
+    @given(auto_data_sets)
+    def test_adapt_data_set_with_auto_detect(self, setup):
+        data_set, kind = setup
+        container = adapt2cuds(data_set, 'test')
+        self.assertIsInstance(container, expected[kind])
+        self.assertIs(container.data_set, data_set)
