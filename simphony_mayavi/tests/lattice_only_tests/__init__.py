c5596234269a7fdd2740da7fd2344bb070f8f8f0
Kit Yan Choi <kchoi@enthought.com>
2015-11-26 17:38:28 +0000

    edited cuds.tests.test_vtk_lattice and added it to lattice_only_tests suite

--- simphony_mayavi/tests/lattice_only_tests/__init__.py
+++ simphony_mayavi/tests/lattice_only_tests/__init__.py
@@ -5,11 +5,17 @@ from simphony_mayavi.tests.test_load import TestLoad
 from simphony_mayavi.tests.test_plugin_loading import TestPluginLoading
 from simphony_mayavi.tests.test_show import TestShow
 from simphony_mayavi.tests.test_snapshot import TestSnapShot
-
+from simphony_mayavi.cuds.tests.test_vtk_lattice import (
+    TestVTKLatticeNodeOperations, TestVTKLatticeNodeCoordinates,
+    TestVTKLattice)
 
 def load_tests(loader, tests, pattern):
     # customised to remove particle and mesh
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestAdapt2Cuds)
+    suite = unittest.TestSuite([
+            unittest.TestLoader().loadTestsFromTestCase(TestAdapt2Cuds),
+            unittest.TestLoader().loadTestsFromTestCase(TestVTKLatticeNodeOperations),
+            unittest.TestLoader().loadTestsFromTestCase(TestVTKLatticeNodeCoordinates),
+            unittest.TestLoader().loadTestsFromTestCase(TestVTKLattice)])
 
     suite.addTest(TestLoad("test_load_vase_1comp_vti"))
     suite.addTest(TestPluginLoading("test_import"))
