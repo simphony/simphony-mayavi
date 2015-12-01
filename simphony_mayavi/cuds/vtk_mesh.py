20196942b67a0b44feaad57465a92db0fb129c56
Kit Yan Choi <kchoi@enthought.com>
2015-12-01 11:23:44 +0000

    revert back to the original vtk_mesh.data setter

--- simphony_mayavi/cuds/vtk_mesh.py
+++ simphony_mayavi/cuds/vtk_mesh.py
@@ -191,10 +191,8 @@ class VTKMesh(ABCMesh):
         return DataContainer(self._data)
 
     @data.setter
-    def data(self, new_data):
-        self._data = DataContainer({key: value
-                                    for key, value in new_data.items()
-                                    if key in self.supported_cuba})
+    def data(self, data):
+        self._data = DataContainer(data)
 
     def count_of(self, item_type):
         def count_element(type_):
