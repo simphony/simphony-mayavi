44d6c1a08f3d81107b1c22d64dd90862c4f3e006
Kit Yan Choi <kchoi@enthought.com>
2015-12-02 15:49:24 +0000

    fixed examples for particles

--- examples/particles_vtk_example.py
+++ examples/particles_vtk_example.py
@@ -9,17 +9,17 @@ bonds = array([[0, 1], [0, 3], [1, 3, 2]])
 temperature = array([10., 20., 30., 40.])
 
 particles = mayavi_tools.VTKParticles('test')
-uids = []
-for index, point in enumerate(points):
-    uid = particles.add_particle(
-        Particle(
-            coordinates=point,
-            data=DataContainer(TEMPERATURE=temperature[index])))
-    uids.append(uid)
 
-for indices in bonds:
-    particles.add_bond(Bond(particles=[uids[index] for index in indices]))
+# add particles
+particle_iter = (Particle(coordinates=point,
+                          data=DataContainer(TEMPERATURE=temperature[index]))
+                 for index, point in enumerate(points))
+uids = particles.add_particles(particle_iter)
 
+# add bonds
+bond_iter = (Bond(particles=[uids[index] for index in indices])
+             for indices in bonds)
+particles.add_bonds(bond_iter)
 
 if __name__ == '__main__':
 
