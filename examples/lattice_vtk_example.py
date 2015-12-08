54257b8327f403d7871f898f465c933ca4b6c480
Kit Yan Choi <kchoi@enthought.com>
2015-12-08 13:14:27 +0000

    Revert "make lattice examples consistent with the image shown on doc"
    This reverts commit 81756824f547c02010e5a1ba3fa009fa53ea77d2.

--- examples/lattice_vtk_example.py
+++ examples/lattice_vtk_example.py
@@ -6,9 +6,17 @@ from simphony.visualisation import mayavi_tools
 
 cubic = mayavi_tools.VTKLattice.empty(
     "test", PrimitiveCell.for_cubic_lattice(0.1),
-    (5, 10, 12), (0, 0, 0))
+    (5, 10, 12), (5, 4, 0))
 
-lattice = cubic
+hexagonal = mayavi_tools.VTKLattice.empty(
+    "test", PrimitiveCell.for_hexagonal_lattice(0.1, 0.05),
+    (5, 10, 12), (5, 4, 0))
+
+bcc = mayavi_tools.VTKLattice.empty(
+    "test", PrimitiveCell.for_body_centered_cubic_lattice(0.1),
+    (5, 10, 12), (5, 4, 0))
+
+lattice = bcc
 
 new_nodes = []
 for node in lattice.iter_nodes():
