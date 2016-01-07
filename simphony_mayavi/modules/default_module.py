9f4cc6d46b62ad274458c1a8f7af44d8da0788cc
Kit Yan Choi <kchoi@enthought.com>
2016-01-07 16:34:37 +0000

    added default module for bonds

--- simphony_mayavi/modules/default_module.py
+++ simphony_mayavi/modules/default_module.py
@@ -5,6 +5,8 @@ from mayavi.tools.tools import _typical_distance
 from tvtk.tvtk_classes.sphere_source import SphereSource
 
 from simphony.cuds.abc_mesh import ABCMesh
+from simphony.cuds.abc_particles import ABCParticles
+from simphony.core.cuds_item import CUDSItem
 
 
 def default_vector_module(scale_factor=1.):
@@ -30,6 +32,12 @@ def default_scalar_module(scale_factor=1.):
     return module
 
 
+def default_bond_module():
+    module = Surface()
+    module.actor.mapper.scalar_visibility = False
+    return module
+
+
 def default_module(source):
     ''' Mapping for module appropriate for the selected data
 
@@ -45,17 +53,29 @@ def default_module(source):
 
     scale_factor = _typical_distance(source.data) * 0.5
 
+    # default module for point scalars
     if source.point_scalars_name:
         if isinstance(source.cuds, ABCMesh):
             modules.append(Surface())
         else:
             modules.append(default_scalar_module(scale_factor))
+
+    # default module for point vectors
     if source.point_vectors_name:
         modules.append(default_vector_module())
+
+    # default module for cell scalars
     if source.cell_scalars_name:
         modules.append(Surface())
+
+    # default module for cell vectors
     if source.cell_vectors_name:
         modules.append(default_vector_module())
+
+    # default module for particle bonds
+    if (isinstance(source.cuds, ABCParticles) and
+            source.cuds.count_of(CUDSItem.BOND) > 0):
+        modules.append(default_bond_module())
 
     if modules:
         return modules
