0c762aab05f1b7f1dcf3c1399aa67542ebe436f0
Kit Yan Choi <kchoi@enthought.com>
2016-01-20 09:41:28 +0000

    added DEFAULT_ENGINES for loader.get_factories

--- simphony_mayavi/plugins/engine_wrappers/loader.py
+++ simphony_mayavi/plugins/engine_wrappers/loader.py
@@ -4,8 +4,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_ENGINES = ("lammps_md", "kratos", "openfoam", "jyulb")
 
-def get_factories(submodules=("lammps_md", "kratos", "openfoam", "jyulb"),
+
+def get_factories(submodules=DEFAULT_ENGINES,
                   name="simphony_mayavi.plugins.engine_wrappers"):
     ''' Return a dictionary containing factory functions for creating
     engine wrappers.  Collect results of name.submodule.get_factories()
@@ -15,8 +17,7 @@ def get_factories(submodules=("lammps_md", "kratos", "openfoam", "jyulb"),
     Parameters
     ----------
     submodules : sequence
-        a list of submodule names (str) to be loaded from
-        ``name``
+        a list of submodule names (str) to be loaded
     name : str
         the module where submodules are loaded from
 
@@ -33,8 +34,8 @@ def get_factories(submodules=("lammps_md", "kratos", "openfoam", "jyulb"),
         try:
             importlib.import_module(module)
         except ImportError as exception:
-            logger.warn(exception.message)
-            logger.warn("Failed to load {}".format(module))
+            logger.warning(exception.message)
+            logger.warning("Failed to load {}".format(module))
         else:
             factories.update(sys.modules[module].get_factories())
     return factories
