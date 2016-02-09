fe2377afff6bb455ce3111c5e49bcf7c779faab5
Kit Yan Choi <kchoi@enthought.com>
2016-02-09 17:08:22 +0000

    expose get_mayavi2_engine_manager and add_engine_to_mayavi2 to plugin

--- simphony_mayavi/api.py
+++ simphony_mayavi/api.py
@@ -2,5 +2,8 @@ from .show import show
 from .snapshot import snapshot
 from .adapt2cuds import adapt2cuds
 from .load import load
+from .get_mayavi2_engine_manager import (get_mayavi2_engine_manager,
+                                         add_engine_to_mayavi2)
 
-__all__ = ['show', 'snapshot', 'adapt2cuds', 'load']
+__all__ = ['show', 'snapshot', 'adapt2cuds', 'load',
+           'get_mayavi2_engine_manager', 'add_engine_to_mayavi2']
--- simphony_mayavi/plugin.py
+++ simphony_mayavi/plugin.py
@@ -1,11 +1,15 @@
 from simphony_mayavi._version import full_version as __version__
-from simphony_mayavi.api import show, snapshot, adapt2cuds, load
+from simphony_mayavi.api import (show, snapshot, adapt2cuds, load,
+                                 get_mayavi2_engine_manager,
+                                 add_engine_to_mayavi2)
 from simphony_mayavi.cuds.api import VTKParticles, VTKLattice, VTKMesh
 from simphony_mayavi.plugins.api import (EngineManagerStandalone,
                                          EngineManagerStandaloneUI)
 
 __all__ = [
     'show', 'snapshot', 'adapt2cuds', 'load',
+    'get_mayavi2_engine_manager',
+    'add_engine_to_mayavi2',
     '__version__',
     'VTKParticles', 'VTKLattice', 'VTKMesh',
     'EngineManagerStandalone', 'EngineManagerStandaloneUI']
