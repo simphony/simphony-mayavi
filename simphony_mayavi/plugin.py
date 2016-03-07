b32d4a702ed703d5562b9d9a9d935dbfb52a3d5c
Kit Yan Choi <kchoi@enthought.com>
2016-03-07 11:26:15 +0000

    this commit actually exposes get_simphony_panel to the plugin

--- simphony_mayavi/plugin.py
+++ simphony_mayavi/plugin.py
@@ -1,13 +1,14 @@
 from simphony_mayavi._version import full_version as __version__
 from simphony_mayavi.api import (show, snapshot, adapt2cuds, load,
-                                 restore_scene, add_engine_to_mayavi2)
+                                 restore_scene, add_engine_to_mayavi2,
+                                 get_simphony_panel)
 from simphony_mayavi.cuds.api import VTKParticles, VTKLattice, VTKMesh
 from simphony_mayavi.plugins.api import (EngineManagerStandalone,
                                          EngineManagerStandaloneUI)
 
 __all__ = [
     'show', 'snapshot', 'adapt2cuds', 'load',
-    'restore_scene', 'add_engine_to_mayavi2',
+    'restore_scene', 'add_engine_to_mayavi2', 'get_simphony_panel',
     '__version__',
     'VTKParticles', 'VTKLattice', 'VTKMesh',
     'EngineManagerStandalone', 'EngineManagerStandaloneUI']
