0c7bd8324faa148221f625b8ccba3e6561f91077
Kit Yan Choi <kchoi@enthought.com>
2016-03-07 11:23:44 +0000

    expose get_simphony_panel to the plugin namespace

--- simphony_mayavi/api.py
+++ simphony_mayavi/api.py
@@ -3,7 +3,9 @@ from .snapshot import snapshot
 from .adapt2cuds import adapt2cuds
 from .load import load
 from .get_mayavi2_engine_manager import add_engine_to_mayavi2
+from .get_mayavi2_engine_manager import get_simphony_panel
 from .restore_scene import restore_scene
 
 __all__ = ['show', 'snapshot', 'adapt2cuds', 'load',
-           'restore_scene', 'add_engine_to_mayavi2']
+           'restore_scene',
+           'get_simphony_panel', 'add_engine_to_mayavi2']
