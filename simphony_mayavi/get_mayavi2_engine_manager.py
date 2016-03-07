1b4bba71c302fd5de750579a53144a49ac755267
Kit Yan Choi <kchoi@enthought.com>
2016-03-07 09:21:51 +0000

    renamed get_mayavi2_engine_manager to get_simphony_panel to be more user friendly

--- simphony_mayavi/get_mayavi2_engine_manager.py
+++ simphony_mayavi/get_mayavi2_engine_manager.py
@@ -1,22 +1,45 @@
-from mayavi import mlab
+from mayavi.tools.engine_manager import get_engine as get_mayavi_engine
+from mayavi.tools.engine_manager import options as mayavi_options
 
 
-def get_mayavi2_engine_manager():
-    ''' Return the EngineManager panel sitting in Mayavi2'''
+def get_simphony_panel():
+    ''' Return the SimPhoNy panel object sitting in Mayavi2
+
+    Returns
+    -------
+    panel : :class:`simphony_mayavi.plugins.engine_manager.EngineManager`
+
+    Raises
+    ------
+    AttributeError
+        If the Mayavi2 application window is not found or
+        the SimPhoNy panel is not found
+    '''
+    # Ensure the Mayavi Engine is an EnvisageEngine
+    old_backend = mayavi_options.backend
+    mayavi_options.backend = "envisage"
+
+    # Look for the application window from the EnvisageEngine
     try:
-        window = mlab.get_engine().window
+        window = get_mayavi_engine().window
     except AttributeError:
         message = "Failed to find the application window. Is Mayavi2 running?"
         raise AttributeError(message)
+    finally:
+        # Whatever happens, reset the backend
+        mayavi_options.backend = old_backend
 
+    # The Simphony Panel (plugin) should be a registered service
     panel = window.get_service("simphony_mayavi.plugins.engine_manager_mayavi2.EngineManagerMayavi2")  # noqa
 
     if panel:
         return panel
     else:
-        message = ("Could not locate the EngineManager contributed by "
+        # It is possible if the user has not activated the
+        # simphony_mayavi plugin in Mayavi2
+        message = ("Could not locate the SimPhoNy panel contributed by "
                    "the simphony_mayavi plugin")
-        raise ValueError(message)
+        raise AttributeError(message)
 
 
 def add_engine_to_mayavi2(name, engine):
@@ -27,7 +50,8 @@ def add_engine_to_mayavi2(name, engine):
     ----------
     name : str
         Name of the Simphony Modeling Engine
+
     engine : ABCModelingEngine
         Simphony Modeling Engine
     '''
-    get_mayavi2_engine_manager().add_engine(name, engine)
+    get_simphony_panel().add_engine(name, engine)
