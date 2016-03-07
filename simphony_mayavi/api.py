a5e59612e15a0b31142f273d3cea6e8a4ad4d868
Kit Yan Choi <kchoi@enthought.com>
2016-03-07 11:31:41 +0000

    renamed get_mayavi2_engine_manager.py to mayavi2.py

--- simphony_mayavi/api.py
+++ simphony_mayavi/api.py
@@ -2,8 +2,8 @@ from .show import show
 from .snapshot import snapshot
 from .adapt2cuds import adapt2cuds
 from .load import load
-from .get_mayavi2_engine_manager import add_engine_to_mayavi2
-from .get_mayavi2_engine_manager import get_simphony_panel
+from .mayavi2 import add_engine_to_mayavi2
+from .mayavi2 import get_simphony_panel
 from .restore_scene import restore_scene
 
 __all__ = ['show', 'snapshot', 'adapt2cuds', 'load',
