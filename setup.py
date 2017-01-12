b8b210dcdea57f572ab9368cd1dc9866b97eec96
Stefano Borini <sborini@enthought.com>
2017-01-12 14:36:03 +0000

    Upgraded setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if on_rtd:
     requirements = ["traits"],
 else:
     requirements = [
-        "simphony[H5IO]~=0.4",
+        "simphony[H5IO]>0.4,<0.6",
         "mayavi[app]==4.4.4"
         ],
 
